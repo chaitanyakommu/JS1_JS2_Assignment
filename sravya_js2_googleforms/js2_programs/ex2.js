f16c290e06fa34482cce991f4d91747801d61dde
Sravyageneration <sravya.it24@gmail.com>
2023-04-11 23:11:22 +1000

    google and exercises

---
+++ sravya_js2_googleforms/js2_programs/ex2.js
@@ -0,0 +1,17 @@
+// The following code takes a given name, runs a function to create a string and logs the name.
+// The output should be "My name is Zara Williams !" when run.
+// The code has 7 errors, find them and fix them and use node to check it is working.
+// Hint: check particularly for incorrect marks/syntax, mispelled functions, and that information is returned from a function if needed.
+
+function concatenate(first, last) {
+    
+    console.log(`My name is ${first} ${last} !`);
+   
+  }
+  function getName(first, last) {
+    let result;
+    result = concatenate(first, last);
+   
+  }
+  getName("Zara", "Williams");
+  
